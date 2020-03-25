1daf0fb0675aeae9874497dfd9195b821fe19ece
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 14:01:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def long_planeteer_calls(calls)
   calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  list.find{|item| cheese_types.include?(item)}
 end
